7eb51734cbb616c1acf8b94737d8298822123e04
Marvin Löbel <loebel.marvin@gmail.com>
2014-07-19 20:59:58 +0200

    Initial code commit

---
+++ src/lib.rs
@@ -0,0 +1,160 @@
+/*!
+Attribute for recursivly applying `pub` modifiers to an tree of items.
+
+# Examples
+
+To load the extension and use it:
+
+```rust,ignore
+#[phase(plugin)]
+extern crate apply_pub;
+
+#[apply_pub(descendants)]
+mod foo {
+    fn bar() {}
+    mod baz {
+        fn qux() {}
+    }
+}
+
+fn main() {
+    foo::bar();
+    for::baz::qux();
+}
+```
+
+*/
+
+#![crate_name = "apply-pub-rs"]
+#![crate_type = "dylib"]
+#![license = "MIT"]
+#![feature(plugin_registrar, managed_boxes)]
+#![feature(phase)]
+
+extern crate syntax;
+extern crate rustc;
+
+use syntax::ast::ViewItem;
+use syntax::ast::{MetaItem, Item, StructField, ViewItemUse, ForeignItem};
+use syntax::ast::{Method, MethDecl, ItemMac, UnnamedField, NamedField};
+use syntax::ast::{Public, ItemFn, ItemImpl, ItemForeignMod, ViewItemExternCrate};
+use syntax::codemap::Span;
+use syntax::ext::base::{ExtCtxt, ItemModifier};
+use syntax::fold::Folder;
+use syntax::fold;
+use syntax::parse::token;
+use syntax::util::small_vector::SmallVector;
+
+use rustc::plugin::Registry;
+
+use std::gc::{Gc, GC};
+
+static NAME: &'static str = "apply_pub";
+
+#[plugin_registrar]
+pub fn plugin_registrar(reg: &mut Registry) {
+    reg.register_syntax_extension(token::intern(NAME), ItemModifier(expand));
+}
+
+struct ApplyPubFolder {
+    parent_item_variant: ItemVariant
+}
+
+fn expand(ctxt: &mut ExtCtxt, span: Span, _meta: Gc<MetaItem>, item: Gc<Item>) -> Gc<Item> {
+    match item.node {
+        ItemMac(..) => {
+            ctxt.span_err(span,
+                format!("Can not apply `#[{}]` to a macro invocation", NAME).as_slice());
+            item
+        }
+        _ => {
+            let expanded = ctxt.expander().fold_item(item).move_iter().nth(0).unwrap();
+            let mut folder = ApplyPubFolder { parent_item_variant: IsOther };
+            folder.fold_item(expanded).move_iter().nth(0).unwrap()
+        }
+    }
+}
+
+enum ItemVariant {
+    IsFn,
+    IsTraitImpl,
+    IsTypeImpl,
+    IsExternBlock,
+    IsOther,
+}
+
+impl ItemVariant {
+    fn is_fn(self) -> bool { match self { IsFn => true, _ => false } }
+    fn is_trait_impl(self) -> bool { match self { IsTraitImpl => true, _ => false } }
+    fn is_type_impl(self) -> bool { match self { IsTypeImpl => true, _ => false } }
+    fn is_extern_block(self) -> bool { match self { IsExternBlock => true, _ => false } }
+    fn can_be_made_pub(self) -> bool {
+        !self.is_trait_impl() && !self.is_type_impl() && !self.is_extern_block()
+    }
+}
+
+impl Folder for ApplyPubFolder {
+    fn fold_view_item(&mut self, vi: &ViewItem) -> ViewItem {
+        let item = fold::noop_fold_view_item(vi, self);
+        match item.node {
+            ViewItemExternCrate(..) => item,
+            ViewItemUse(..) => ViewItem { vis: Public, ..item },
+        }
+    }
+
+    fn fold_item(&mut self, i: Gc<Item>) -> SmallVector<Gc<Item>> {
+        let parent_item_variant = self.parent_item_variant;
+
+        let item_variant = match i.node {
+            ItemFn(..) => IsFn,
+            ItemImpl(_, None, _, _) => IsTypeImpl,
+            ItemImpl(_, _, _, _) => IsTraitImpl,
+            ItemForeignMod(_) => IsExternBlock,
+            _ => IsOther,
+        };
+
+        self.parent_item_variant = item_variant;
+        let items = fold::noop_fold_item(&*i, self);
+        self.parent_item_variant = parent_item_variant;
+
+        items.move_iter().map(|item| {
+            let mut item = (*item).clone();
+            if !parent_item_variant.is_fn() && item_variant.can_be_made_pub() {
+                item.vis = Public;
+            }
+            box(GC) item
+        }).collect()
+    }
+
+    fn fold_struct_field(&mut self, sf: &StructField) -> StructField {
+        let mut field = fold::noop_fold_struct_field(sf, self);
+        match field.node.kind {
+            NamedField(_, ref mut vis) => *vis = Public,
+            UnnamedField(ref mut vis) => *vis = Public,
+        }
+        field
+    }
+
+    fn fold_method(&mut self, m: Gc<Method>) -> SmallVector<Gc<Method>>  {
+        let methods = fold::noop_fold_method(&*m, self);
+        if !self.parent_item_variant.is_trait_impl() {
+            methods.move_iter().map(|method| {
+                let mut method = (*method).clone();
+                match method.node {
+                    MethDecl(_, _, _, _, _, _, _, ref mut vis) => *vis = Public,
+                    _ => (),
+                }
+                box(GC) method
+            }).collect()
+        } else {
+            methods
+        }
+    }
+
+    fn fold_foreign_item(&mut self, ni: Gc<ForeignItem>) -> Gc<ForeignItem> {
+        let foreign_item = fold::noop_fold_foreign_item(&*ni, self);
+        let mut foreign_item = (*foreign_item).clone();
+        foreign_item.vis = Public;
+        box (GC) foreign_item
+    }
+}
---
+++ tests/run-pass.rs
@@ -0,0 +1,143 @@
+#![feature(phase, macro_rules, struct_variant)]
+
+#[phase(plugin)]
+extern crate apply_pub = "apply-pub-rs";
+extern crate libc;
+use libc::c_char;
+
+#[apply_pub]
+mod foo {
+    extern crate core;
+    use baz2 = self::baz;
+    use self::baz::Foo;
+    fn bar() { let _: uint = Foo::foo_fn();  }
+    mod baz {
+        fn qux() {
+            mod foo {
+                struct Bar;
+                impl Bar {
+                    fn bar() {}
+                }
+            }
+            foo::Bar::bar();
+        }
+        static FOO: uint = 0u;
+        static BAR: uint = {
+            mod bar {
+                static BAR: uint = 100;
+            }
+            bar::BAR
+        };
+        struct Data {
+            a: uint,
+            b: ()
+        }
+        enum Enum {
+            Var1,
+            Var2(uint, uint),
+            Var3 { a: uint },
+        }
+        trait Foo {
+            fn foo_meth(&self);
+            fn foo_fn() -> Self;
+        }
+        impl Foo for Data {
+            fn foo_meth(&self){}
+            fn foo_fn() -> Data { Data { a: 0, b: () } }
+        }
+        type NewData = Data;
+        struct Data2;
+        struct Data3(uint, uint);
+        extern {
+            fn toupper(c: ::libc::c_char) -> ::libc::c_char;
+        }
+    }
+}
+
+fn test() {
+    use foo::baz::Foo;
+
+    foo::bar();
+    foo::baz::qux();
+    let _ = foo::baz::FOO;
+    let data = foo::baz::Data { a: 0u, b: () };
+    data.foo_meth();
+    let _: foo::baz::NewData = foo::baz::Foo::foo_fn();
+
+    let _ = foo::baz::Data2;
+    let _ = foo::baz::Data3(100u, 200u);
+
+    unsafe {
+        assert_eq!(b'A' as c_char, foo::baz::toupper(b'a' as c_char));
+    }
+
+    {
+        use foo::baz::{Enum, Var1, Var2, Var3};
+        let _: Enum = Var1;
+        let _ = Var2(0, 1);
+        let _ = Var3 { a: 100 };
+    }
+
+    impl foo::Foo for uint {
+        fn foo_meth(&self) {}
+        fn foo_fn() -> uint { 0 }
+    }
+}
+
+#[apply_pub]
+fn a() {
+    mod am {
+        fn x() {}
+    }
+    fn b() {
+        mod bm {
+            fn x() {}
+        }
+        fn c() {
+            mod cm {
+                fn x() {}
+            }
+            fn d() {
+                mod dm {
+                    fn x() {}
+                }
+                dm::x()
+            }
+            cm::x();
+            d();
+        }
+        bm::x();
+        c();
+    }
+    am::x();
+    b();
+}
+
+macro_rules! items {
+    () => {
+        fn b(){}
+        mod a {}
+    }
+}
+
+#[apply_pub]
+mod test_in_mod {
+    items!()
+}
+
+// compile_fail:
+// macro_rules! rec_items {
+//     () => {
+//         fn rec_b(){ rec_a(); }
+//         fn rec_a(){ rec_b(); }
+//     }
+// }
+// #[apply_pub]
+// rec_items!()
+
+#[test]
+fn main_test() {
+    test();
+    a();
+    println!("OK")
+}
