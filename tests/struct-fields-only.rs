0445ede1d9e1504d776e78b754da6a2764934491
Marvin Löbel <loebel.marvin@gmail.com>
2014-08-02 02:42:57 +0200

    Add another example

---
+++ tests/struct-fields-only.rs
@@ -0,0 +1,26 @@
+#![feature(phase)]
+
+#[phase(plugin)]
+extern crate apply_pub = "apply-pub-rs";
+
+mod foo {
+    #[apply_pub]
+    struct Foo {
+        a: uint,
+        b: uint,
+        c: uint,
+        d: uint,
+        e: uint,
+    }
+}
+
+#[test]
+fn main() {
+    let _ = foo::Foo {
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0,
+        e: 0,
+    };
+}
